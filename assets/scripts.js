40140190bbc514bcf3c9e953b73eaf7ebb5a59af
David Tunnell <davidtunnell9@gmail.com>
2021-10-19 13:47:45 -0500

    update api key

--- assets/scripts.js
+++ assets/scripts.js
@@ -18,21 +18,21 @@ var currentYear = moment().year();
 //these would typically not be shown on the front end
 var apiKeyDiscogs = "ZkPKfcbrCFxLTLxNSjiZlgnTrLWdqMuIPPYUvVMx";
 //if quota is reached, create a new project then api key here (update project name in URL): https://console.developers.google.com/apis/api/youtube.googleapis.com/overview?project=452704620540 and then update variable
-var apiKeyYouTube = "AIzaSyBxU5VtK-dQ8pPxbKyBu-R30xOa9BxMoeE";
+var apiKeyYouTube = "AIzaSyDdPPp3lg3nt_5w6B1KAOkosMq-ZfV9OSc";
 //max search history
 var maxNumberOfMenuItems = 8;
 //set DOM year
 yearEl.innerHTML = currentYear;
 
 //event listeners
-searchButtonEl.addEventListener("click", function() {
+searchButtonEl.addEventListener("click", function () {
     var userEntry = searchUserInputEl.value;
     addRecentSearchButton(userEntry);
     getSearchResults(100, userEntry, apiKeyDiscogs);
 });
 
-searchUserInputEl.addEventListener("keyup", function(event) {
-    if (event.key === 'Enter') {
+searchUserInputEl.addEventListener("keyup", function (event) {
+    if (event.key === "Enter") {
         var userEntry = searchUserInputEl.value;
         addRecentSearchButton(userEntry);
         getSearchResults(100, userEntry, apiKeyDiscogs);
@@ -40,24 +40,24 @@ searchUserInputEl.addEventListener("keyup", function(event) {
 });
 
 //when clicking search a history button
-searchHistoryEl.addEventListener("click", function(event) {
+searchHistoryEl.addEventListener("click", function (event) {
     var userEntry = event.target.innerHTML;
     //need to search here and return albums
     getSearchResults(100, userEntry, apiKeyDiscogs);
 });
 
 //when clicking the exit modal button
-exitModalButtonEl.addEventListener("click", function(event) {
+exitModalButtonEl.addEventListener("click", function (event) {
     modalInfoEl.classList.remove("is-active");
 });
 
 //when clicking modal background
-modalBackgroundEl.addEventListener("click", function(event) {
+modalBackgroundEl.addEventListener("click", function (event) {
     modalInfoEl.classList.remove("is-active");
 });
 
 //when clicking a trending singles card
-trendingSinglesEl.addEventListener("click", function(event) {
+trendingSinglesEl.addEventListener("click", function (event) {
     var userEntry = event.target.closest(".modal-click");
     var selectedCard = event.target.closest(".has-text-centered");
     if (userEntry) {
@@ -67,7 +67,7 @@ trendingSinglesEl.addEventListener("click", function(event) {
 });
 
 //when clicking record that was returned
-recommendationsEl.addEventListener("click", function(event) {
+recommendationsEl.addEventListener("click", function (event) {
     var userEntry = event.target.closest(".modal-click");
     var selectedCard = event.target.closest(".has-text-centered");
     if (userEntry) {
@@ -87,8 +87,16 @@ function populateModal(selectedCard) {
     var genres = mediaContent.children[1].innerHTML;
     var label = mediaContent.children[2].innerHTML;
     //generate and add to DOM
-    var html = "<img class='modal-image' src='" +
-        image + "' alt='" + title + "'><p>Genre(s): " + genres + " | Label: " + label + "</p>";
+    var html =
+        "<img class='modal-image' src='" +
+        image +
+        "' alt='" +
+        title +
+        "'><p>Genre(s): " +
+        genres +
+        " | Label: " +
+        label +
+        "</p>";
     modalContentEl.innerHTML = html;
     if (title.length > maxCharLength) {
         title = title.substring(0, maxCharLength);
@@ -101,10 +109,13 @@ function addRecentSearchButton(userEntry) {
     //create object to add to local storage array
     var recentSearch = {
         input: userEntry,
-        genres: ""
+        genres: "",
     };
     //add to DOM
-    var html = "<button class='button is-info is-medium is-fullwidth'>" + recentSearch.input + "</button>";
+    var html =
+        "<button class='button is-info is-medium is-fullwidth'>" +
+        recentSearch.input +
+        "</button>";
     //add to DOM at the beginning of the element
     searchHistoryEl.insertAdjacentHTML("afterbegin", html);
     //if the max number is reached, remove the last item from the DOM
@@ -132,24 +143,30 @@ function populateRecentlySearched() {
     var existingEntries = JSON.parse(localStorage.getItem("allSavedSearches"));
     if (existingEntries != null) {
         for (let i = 0; i < existingEntries.length; i++) {
-            searchHistoryEl.innerHTML += "<button class='button is-info is-medium is-fullwidth'>" + existingEntries[i].input + "</button>";;
+            searchHistoryEl.innerHTML +=
+                "<button class='button is-info is-medium is-fullwidth'>" +
+                existingEntries[i].input +
+                "</button>";
         }
     }
 }
 
 //get recommendations from YouTube
 function getYouTubeRecommendations(userEntry, NumOfResults, apiKey) {
-    var constructedUrl = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=" + NumOfResults +
+    var constructedUrl =
+        "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=" +
+        NumOfResults +
         "&order=relevance&q=" +
         userEntry +
-        "&key=" + apiKey;
+        "&key=" +
+        apiKey;
     var results = getYouTubeApi(constructedUrl);
     results.then((data) => {
         updateYouTubeDom(data);
     });
 }
 
-//update DOM with YouTube data 
+//update DOM with YouTube data
 function updateYouTubeDom(results) {
     shuffleArray(results);
     var html = "";
@@ -159,17 +176,23 @@ function updateYouTubeDom(results) {
     }
     for (let i = 0; i < maxLength; i++) {
         const ele = results[i];
-        html = "<div class='column is-one-fifth has-text-centered'><a href='https://www.youtube.com/watch?v=" +
-            ele.videoId + "' target='_blank'><div class='card large'><div class='card-image'><figure class='image'><img src='" +
-            ele.image.url + "' alt='" +
-            ele.title + "'></figure></div><div class='card-content'><div class='media'><div class='media-content'><p class='card-title'>" +
-            ele.title + "</p><p class='card-info'>" +
-            ele.created + "</p></div></div></div></div></a></div>";
+        html =
+            "<div class='column is-one-fifth has-text-centered'><a href='https://www.youtube.com/watch?v=" +
+            ele.videoId +
+            "' target='_blank'><div class='card large'><div class='card-image'><figure class='image'><img src='" +
+            ele.image.url +
+            "' alt='" +
+            ele.title +
+            "'></figure></div><div class='card-content'><div class='media'><div class='media-content'><p class='card-title'>" +
+            ele.title +
+            "</p><p class='card-info'>" +
+            ele.created +
+            "</p></div></div></div></div></a></div>";
         recommendationsEl.innerHTML += html;
     }
 }
 
-//load youtube recommendations from genres saved to local storage 
+//load youtube recommendations from genres saved to local storage
 function onLoadYouTubeRecommendations(apiKey) {
     // Parse any JSON previously stored in allEntries
     var existingGenres = JSON.parse(localStorage.getItem("savedGenres"));
@@ -185,25 +208,29 @@ function onLoadYouTubeRecommendations(apiKey) {
 async function getYouTubeApi(constructedUrl) {
     var resultObjects = [];
     //get a response and then iterate through data, if it is ranked high enough add the object to the array
-    const response = await fetch(constructedUrl).then(response => {
-        if (response.ok) {
-            return response.json();
-        }
-    }).then((data) => {
-        for (let i = 0; i < data.items.length; i++) {
-            var ele = data.items[i];
-            var resultObject = {
-                title: ele.snippet.title,
-                videoId: ele.id.videoId,
-                image: ele.snippet.thumbnails.medium,
-                created: moment(ele.snippet.publishTime).format("M-DD-YYYY"),
-                channelName: ele.snippet.channelTitle
+    const response = await fetch(constructedUrl)
+        .then((response) => {
+            if (response.ok) {
+                return response.json();
             }
-            if (resultObject.videoId) {
-                resultObjects.push(resultObject);
+        })
+        .then((data) => {
+            for (let i = 0; i < data.items.length; i++) {
+                var ele = data.items[i];
+                var resultObject = {
+                    title: ele.snippet.title,
+                    videoId: ele.id.videoId,
+                    image: ele.snippet.thumbnails.medium,
+                    created: moment(ele.snippet.publishTime).format(
+                        "M-DD-YYYY"
+                    ),
+                    channelName: ele.snippet.channelTitle,
+                };
+                if (resultObject.videoId) {
+                    resultObjects.push(resultObject);
+                }
             }
-        }
-    });
+        });
     return resultObjects;
 }
 
@@ -211,9 +238,13 @@ async function getYouTubeApi(constructedUrl) {
 //https://api.discogs.com/database/search?year=2021&format=single&token=ZkPKfcbrCFxLTLxNSjiZlgnTrLWdqMuIPPYUvVMx
 function getTrendingSingles(rankThreshold, year, genre, apiKey) {
     //create string to search by year
-    var constructedUrl = "https://api.discogs.com/database/search?genre=" +
-        genre + "&year=" +
-        year + "&format=single&token=" + apiKey;
+    var constructedUrl =
+        "https://api.discogs.com/database/search?genre=" +
+        genre +
+        "&year=" +
+        year +
+        "&format=single&token=" +
+        apiKey;
     //get the promise back and then once data comes back pass to update DOM
     var results = callDiscogsApi(constructedUrl, rankThreshold);
     results.then((data) => {
@@ -221,7 +252,7 @@ function getTrendingSingles(rankThreshold, year, genre, apiKey) {
     });
 }
 
-//update DOM for trending singles 
+//update DOM for trending singles
 //TODO: similar to updateTrendingAlbumDom - refactor function
 function updateTrendingSinglesDom(resultAlbums) {
     //randomize the array
@@ -246,8 +277,11 @@ function onLoadGetTrendingSingles(apiKey) {
 //https://api.discogs.com/database/search?q=hip+hop&token=ZkPKfcbrCFxLTLxNSjiZlgnTrLWdqMuIPPYUvVMx
 function getSearchResults(rankThreshold, userEntry, apiKey) {
     //create string to search by user entry
-    var constructedUrl = "https://api.discogs.com/database/search?q=" +
-        userEntry + "&token=" + apiKey;
+    var constructedUrl =
+        "https://api.discogs.com/database/search?q=" +
+        userEntry +
+        "&token=" +
+        apiKey;
     //get the promise back and then once data comes back pass to update DOM
     var results = callDiscogsApi(constructedUrl, rankThreshold);
     results.then((data) => {
@@ -259,19 +293,22 @@ function getSearchResults(rankThreshold, userEntry, apiKey) {
 //get a relevant list of genres and save to local storage
 function saveRelevantGenres(resultAlbums) {
     console.log(resultAlbums);
-    const savedGenres = new Set()
-        //this does not get every genre because genres are an array when returned but it's enough
+    const savedGenres = new Set();
+    //this does not get every genre because genres are an array when returned but it's enough
     for (let i = 0; i < resultAlbums.length; i++) {
         const ele = resultAlbums[i];
         savedGenres.add(ele.genre[0]);
     }
     //change set to array and save to local storage
-    localStorage.setItem("savedGenres", JSON.stringify(Array.from(savedGenres)));
+    localStorage.setItem(
+        "savedGenres",
+        JSON.stringify(Array.from(savedGenres))
+    );
 }
 
-//update DOM for trending albums 
+//update DOM for trending albums
 function updateSearchResultsDom(resultAlbums) {
-    mainTextEl.innerHTML = "Search results..."
+    mainTextEl.innerHTML = "Search results...";
     updateDomFromDicogs(resultAlbums, 50, recommendationsEl);
 }
 
@@ -291,12 +328,18 @@ function updateDomFromDicogs(resultAlbums, maxResults, elementUpdated) {
                 genres += " | ";
             }
         }
-        html = "<div class='column is-one-fifth has-text-centered'><div class='card large modal-click'><div class='card-image'><figure class='image'><img class='content-image' src='" +
-            ele.image + "' alt='" +
-            ele.title + "'></figure></div><div class='card-content'><div class='media'><div class='media-content'><p class='card-title'>" +
-            ele.title + "</p><p class='card-info'>" +
-            genres + "</p><p class='card-info'>" +
-            ele.label[0] + "</p></div></div></div></div></div>";
+        html =
+            "<div class='column is-one-fifth has-text-centered'><div class='card large modal-click'><div class='card-image'><figure class='image'><img class='content-image' src='" +
+            ele.image +
+            "' alt='" +
+            ele.title +
+            "'></figure></div><div class='card-content'><div class='media'><div class='media-content'><p class='card-title'>" +
+            ele.title +
+            "</p><p class='card-info'>" +
+            genres +
+            "</p><p class='card-info'>" +
+            ele.label[0] +
+            "</p></div></div></div></div></div>";
         elementUpdated.innerHTML += html;
         genres = "";
     }
@@ -306,37 +349,39 @@ function updateDomFromDicogs(resultAlbums, maxResults, elementUpdated) {
 async function callDiscogsApi(constructedUrl, rankThreshold) {
     var resultObjects = [];
     //get a response and then iterate through data, if it is ranked high enough add the object to the array
-    const response = await fetch(constructedUrl).then(response => {
-        if (response.ok) {
-            return response.json();
-        }
-    }).then((data) => {
-        for (let i = 0; i < data.results.length; i++) {
-            var ele = data.results[i];
-            //rank doesn't always exist
-            var rankTotal = 0;
-            if (ele.community) {
-                rankTotal = ele.community.want + ele.community.have
+    const response = await fetch(constructedUrl)
+        .then((response) => {
+            if (response.ok) {
+                return response.json();
             }
-            var resultObject = {
-                title: ele.title,
-                image: ele.cover_image,
-                rank: rankTotal,
-                genre: ele.genre,
-                label: ele.label
-            };
-            //filters out those that don't have sufficient rank calculated above
-            if (resultObject.rank > rankThreshold) {
-                resultObjects.push(resultObject);
+        })
+        .then((data) => {
+            for (let i = 0; i < data.results.length; i++) {
+                var ele = data.results[i];
+                //rank doesn't always exist
+                var rankTotal = 0;
+                if (ele.community) {
+                    rankTotal = ele.community.want + ele.community.have;
+                }
+                var resultObject = {
+                    title: ele.title,
+                    image: ele.cover_image,
+                    rank: rankTotal,
+                    genre: ele.genre,
+                    label: ele.label,
+                };
+                //filters out those that don't have sufficient rank calculated above
+                if (resultObject.rank > rankThreshold) {
+                    resultObjects.push(resultObject);
+                }
             }
-        }
-    });
+        });
     return resultObjects;
 }
 
 //replace space with plus to pass to API URLs
 function replaceSpaceWithPlus(str) {
-    return str.replace(/\s+/g, '+');
+    return str.replace(/\s+/g, "+");
 }
 
 //Shuffles an array to randomize
